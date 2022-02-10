50a5cad2f7868c6f6427a015fbf7fe12a58da3f6
Markeisha <markeisha94@gmail.com>
2022-02-10 13:26:05 -0600

    json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const express = require('express');
+const server = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}!`);
+    console.log(process.env.MESSAGE);
+});
+
+server.use('*', (req, res) => {
+    res.send('<h1>Hello, Heroku!</h1>');
+});
